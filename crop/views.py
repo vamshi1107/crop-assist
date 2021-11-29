2469d2992d7c168e55d82b34a8c859c4a13fe66e
Vamshi Addanki <vamshi.a4u@gmail.com>
2021-11-29 20:32:45 +0530

    initial

---
+++ crop/views.py
@@ -0,0 +1,97 @@
+from django.http.response import JsonResponse
+from django.shortcuts import redirect, render
+from crop.values import values, crops, predresult, leafs
+import pickle
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import tensorflow
+
+leafmodel = tensorflow.keras.models.load_model("crop/models/model")
+
+
+def index(request):
+    return render(request, "index.html", {"oplist": values.keys()})
+
+
+def fertresult(request):
+    n = int(request.GET.get("n", 0))
+    p = int(request.GET.get("p", 0))
+    k = int(request.GET.get("k", 0))
+    c = request.GET.get("c", 0)
+    result = check([c, n, p, k])
+    k = result[0]
+    ver = ""
+    if result[1] == "+":
+        k += "High"
+        ver = " IS HIGH"
+    else:
+        k += "low"
+        ver = " IS LOW"
+    return JsonResponse({"result": result[0], "desc": predresult[k], "ver": ver})
+
+
+def check(a):
+    global values
+    imp = ["N", "P", "K"]
+    v = values[a[0]]
+    diff = [i-j for i, j in zip(list(v.values()), a[1:])]
+    if diff[0] == 0 and diff[1] == 0 and diff[2] == 0:
+        return ["all", "="]
+    else:
+        ma = max(diff)
+        mi = min(diff)
+        comp = imp[[abs(i) for i in diff].index(max(abs(ma), abs(mi)))]
+        am = "+" if abs(ma) < abs(mi) else "-"
+        return [comp, am]
+
+
+class crop:
+    def __init__(self, n, p, k, temp, humidity, ph, rain):
+        self.n = n
+        self.p = p
+        self.k = k
+        self.temp = temp
+        self.humidity = humidity
+        self.ph = ph
+        self.rain = rain
+
+    def predict(self):
+        filename = "crop/models/croprecomndation_model.pkl"
+        loaded_model1 = pickle.load(open(filename, 'rb'))
+        return (loaded_model1.predict([[self.n, self.p, self.k, self.temp, self.humidity, self.ph, self.rain]]))
+
+
+def croprecomnder(request):
+    result = [0, 0]
+    if request.method == 'POST':
+        n = float(request.POST.get('Ni'))
+        p = float(request.POST.get('p'))
+        k = float(request.POST.get('k'))
+        temp = float(request.POST.get('temp'))
+        humidity = float(request.POST.get('h'))
+        ph = float(request.POST.get('phvalue'))
+        rain = float(request.POST.get('rainfall'))
+        c = crop(n, p, k, temp, humidity, ph, rain)
+        name = c.predict()[0]
+        result = {}
+        for i in crops:
+            if i["name"].lower() == name.lower():
+                result = i
+        print(name)
+    return JsonResponse({'result': result["name"].upper(), "img": result["img"]})
+
+
+def convert(img):
+    return np.array(Image.open(BytesIO(img)))
+
+
+def leafpred(req):
+    if req.method == "POST":
+        file = req.FILES.get("leaf")
+        img = convert(file.read())
+        img = np.expand_dims(img/255, 0)
+        pred = leafmodel.predict(img)
+        fv = np.argmax(pred[0])
+        name = leafs[fv]
+    return JsonResponse({"name": name})
